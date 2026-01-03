ef28e3313c9d669f314cadcfeec13a3d449308a8
Wesley0010012 <wglaurindo302@gmail.com>
2026-01-02 21:10:47 -0300

    feat: add ColoredMesh

---
+++ src/Core/ColoredMesh/ColoredMesh.hpp
@@ -0,0 +1,14 @@
+#ifndef __COLORED_MESHS_H
+#define __COLORED_MESHS_H
+
+#include "../../Config/MainConfig.hpp"
+
+struct ColoredMesh
+{
+    int meshNumber;
+    float r;
+    float g;
+    float b;
+};
+
+#endif
