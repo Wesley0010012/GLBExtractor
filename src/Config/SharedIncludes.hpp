3d6705c238557b7e5948ed537fbb373fc60aa538
Wesley0010012 <wglaurindo302@gmail.com>
2026-01-02 21:07:37 -0300

    feat: add SharedIncludes

---
+++ src/Config/SharedIncludes.hpp
@@ -0,0 +1,25 @@
+#ifndef __SHARED_INCLUDES_H
+#define __SHARED_INCLUDES_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cfloat>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
+#include "../Libs/tiny_gltf.h"
+#include "../Libs/stb_image_write.h"
+
+#include "../Libs/glad.h"
+#include "../Libs/json.hpp"
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#endif
